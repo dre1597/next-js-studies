6baf43fd553a7d66262ed0b96034f3b3b85deae0
André Pereira <andrepereira97@yahoo.com>
2023-04-10 22:42:50 -0300

    :tada: start project

---
+++ next-clean-arch-fc/src/pages/index.tsx
@@ -0,0 +1,32 @@
+import { GetServerSideProps, NextPage } from 'next';
+import { http } from '@/utils/http';
+import { Product } from '@/utils/models';
+
+type HomeProps = {
+  products: Product[];
+}
+
+const Home: NextPage<HomeProps> = ({ products }) => {
+  return (
+    <div>
+      <h1>Ecommerce</h1>
+      <ul>
+        { products.map((product, key) => (
+          <li key={ key }>
+            <label>Name: </label> { product.name } | <a href="#">Details</a>
+          </li>
+        )) }
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { data: products } = await http.get('products');
+
+  return {
+    props: { products }
+  };
+};
